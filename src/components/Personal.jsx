4eb1058058b29717fe582b0d8761ca8c1e8972e3
Pohe1999 <106699864+Pohe1999@users.noreply.github.com>
2023-11-30 17:01:14 -0600

    Update Personal.jsx

--- src/components/Personal.jsx
+++ src/components/Personal.jsx
@@ -1,146 +1,75 @@
-import React, { useState, useRef } from "react";
-import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
+import React, { useState } from "react";
 
 const Personal = () => {
-  const [currentImages, setCurrentImages] = useState([0, 0, 0, 0, 0, 0, 0, 0]);
-  const touchStartX = useRef(null);
+  const [currentImages] = useState([0, 0, 0, 0, 0, 0, 0, 0]);
 
   const cardsData = [
     {
-        nombre: "hrnndzsamuel",
-        ubicacion: "Tecamac",
-        likes: "Libros",
-        imagenes: [
-          "/galeria/samuel-perfil.jpeg",
-          "src/assets/galeria/galeria-7.jpeg",
-          "src/assets/galeria/galeria-8.jpg"
-        ]
-      },
-      {
-        nombre: "hrnndzsamuel",
-        ubicacion: "Tecamac",
-        likes: "Peliculas",
-        imagenes: [
-          "/galeria/galeria-1.jpeg",
-          "https://picsum.photos/id/201/900/900",
-          "https://picsum.photos/id/202/900/900"
-        ]
-      },
-      {
-          nombre: "hrnndzsamuel",
-          ubicacion: "Tecamac",
-          likes: "Peliculas",
-          imagenes: [
-            "/galeria/galeria-2.jpeg",
-            "https://picsum.photos/id/201/900/900",
-            "https://picsum.photos/id/202/900/900"
-          ]
-        },
-        {
-          nombre: "hrnndzsamuel",
-          ubicacion: "Tecamac",
-          likes: "Peliculas",
-          imagenes: [
-            "/galeria/galeria-3.jpeg",
-            "https://picsum.photos/id/201/900/900",
-            "https://picsum.photos/id/202/900/900"
-          ]
-        },
-        {
-          nombre: "hrnndzsamuel",
-          ubicacion: "Tecamac",
-          likes: "Peliculas",
-          imagenes: [
-            "/galeria/galeria-4.jpeg",
-            "https://picsum.photos/id/201/900/900",
-            "https://picsum.photos/id/202/900/900"
-          ]
-        },
-        {
-          nombre: "hrnndzsamuel",
-          ubicacion: "Tecamac",
-          likes: "Peliculas",
-          imagenes: [
-            "/galeria/galeria-5.jpeg",
-            "https://picsum.photos/id/201/900/900",
-            "https://picsum.photos/id/202/900/900"
-          ]
-        },
-        {
-          nombre: "hrnndzsamuel",
-          ubicacion: "Tecamac",
-          likes: "Peliculas",
-          imagenes: [
-            "/galeria/galeria-6.jpeg",
-            "https://picsum.photos/id/201/900/900",
-            "https://picsum.photos/id/202/900/900"
-          ]
-        },
-        {
-          nombre: "hrnndzsamuel",
-          ubicacion: "Tecamac",
-          likes: "Peliculas",
-          imagenes: [
-            "/galeria/galeria-7.jpeg",
-            "https://picsum.photos/id/201/900/900",
-            "https://picsum.photos/id/202/900/900"
-          ]
-        }
+      nombre: "hrnndzsamuel",
+      ubicacion: "Tecamac",
+      likes: "Peliculas",
+      imagenes: ["/galeria/samuel-perfil.jpeg"],
+      link: "https://www.instagram.com/p/CzAbn-iqfsY/?img_index=1"
+    },
+    {
+      nombre: "hrnndzsamuel",
+      ubicacion: "Tecamac",
+      likes: "Libros",
+      imagenes: ["/galeria/galeria-1.jpeg"]
+    },
+    {
+      nombre: "hrnndzsamuel",
+      ubicacion: "Tecamac",
+      likes: "Deportes",
+      imagenes: ["/galeria/galeria-2.jpeg"]
+    },
+    {
+      nombre: "hrnndzsamuel",
+      ubicacion: "Tecamac",
+      likes: "Musica",
+      imagenes: ["/galeria/galeria-3.jpeg"]
+    },
+    {
+      nombre: "hrnndzsamuel",
+      ubicacion: "Tecamac",
+      likes: "Viajes",
+      imagenes: ["/galeria/galeria-4.jpeg"]
+    },
+    {
+      nombre: "hrnndzsamuel",
+      ubicacion: "Tecamac",
+      likes: "Peliculas",
+      imagenes: ["/galeria/galeria-5.jpeg"]
+    },
+    {
+      nombre: "hrnndzsamuel",
+      ubicacion: "Tecamac",
+      likes: "Peliculas",
+      imagenes: ["/galeria/galeria-6.jpeg"]
+    },
+    {
+      nombre: "hrnndzsamuel",
+      ubicacion: "Tecamac",
+      likes: "Peliculas",
+      imagenes: ["/galeria/galeria-7.jpeg"]
+    }
   ];
 
-  const handleTouchStart = (index, e) => {
-    touchStartX.current = e.touches[0].clientX;
-  };
-
-  const handleTouchEnd = (index, e) => {
-    if (touchStartX.current === null) {
-      return;
-    }
-
-    const touchEndX = e.changedTouches[0].clientX;
-    const deltaX = touchEndX - touchStartX.current;
-
-    if (deltaX > 50) {
-      prevImage(index);
-    } else if (deltaX < -50) {
-      nextImage(index);
-    }
-
-    touchStartX.current = null;
-  };
-
-  const nextImage = (index) => {
-    setCurrentImages((prevImages) => {
-      const newImages = [...prevImages];
-      newImages[index] = (newImages[index] + 1) % cardsData[index].imagenes.length;
-      return newImages;
-    });
-  };
-
-  const prevImage = (index) => {
-    setCurrentImages((prevImages) => {
-      const newImages = [...prevImages];
-      newImages[index] = (newImages[index] - 1 + cardsData[index].imagenes.length) % cardsData[index].imagenes.length;
-      return newImages;
-    })
-  }
-
   return (
-    <div className="bg-gray-100 p-4 mt-20 mb-40">
+    <div className="bg-gray-100 p-4 mt-20 mb-40 pb-10">
       <div className="text-center pb-10">
-        <h2 className='text-4xl font-bold inline border-b-4  border-red-700'>Un poco mas de mi</h2>
+        <h2 className='text-4xl font-bold inline border-b-4 border-red-700'>Un poco mas de mi</h2>
       </div>
       <h3 className="text-center font-thin mb-7 text-xl">
-        Les comparto un poco más de mis gustos personales y pasiones.
+        Les comparto un poco más de mi vida personal y mis pasatiempos.
       </h3>
       <div className="flex overflow-x-scroll sm:px-11 gap-4 sm:justify-start">
         {currentImages.map((currentImage, index) => (
           <div
             key={index}
-            className="bg-white border rounded-2xl max-w-md mb-4 sm:mb-0 sm:w-80 md:w-96"
-            style={{ minWidth: '400px' }}
-            onTouchStart={(e) => handleTouchStart(index, e)}
-            onTouchEnd={(e) => handleTouchEnd(index, e)}
+            className="bg-white border rounded-2xl sm:w-full sm:min-w-0 shadow-2xl
+                        md:w-1/2 lg:w-1/3 xl:w-1/4"  // Agregar estas clases para ajustar el tamaño en diferentes tamaños de pantalla
+            style={{ minWidth: '350px', width: '100%' }}
           >
             <div className="flex items-center px-4 py-3">
               <img className="h-8 w-8 rounded-full" src="/galeria/samuel-perfil.jpeg" alt="Profile" />
@@ -149,25 +78,17 @@ const Personal = () => {
                 <span className="text-gray-600 text-xs block">{cardsData[index].ubicacion}</span>
               </div>
             </div>
-            <div style={{ height: '400px' }}>
+            <div style={{ height: '300px' }}> {/* Ajusta la altura según sea necesario */}
               <img
                 src={cardsData[index].imagenes[currentImage]}
                 alt={`Main ${index}`}
                 style={{ width: '100%', height: '100%', objectFit: 'cover' }}
               />
             </div>
-            <div className="flex items-center justify-between mx-4 mt-3 mb-2">
-              <button onClick={() => prevImage(index)}>
-                <BsChevronCompactLeft size={30} />
-              </button>
-              <div className="flex gap-5">
-                {/* Resto de los SVGs */}
-              </div>
-              <button onClick={() => nextImage(index)}>
-                <BsChevronCompactRight size={30} />
-              </button>
+            <div className="flex justify-between items-center  text-sm mx-4 mt-2 mb-4">
+              <div className="font-semibold">{cardsData[index].likes}</div>
+              <a className="font-thin bg-black rounded-xl text-white px-2 py-1 cursor-pointer" href={cardsData[index].link} target="_blank">Ver mas</a>
             </div>
-            <div className="font-semibold text-sm mx-4 mt-2 mb-4">{cardsData[index].likes}</div>
           </div>
         ))}
       </div>
