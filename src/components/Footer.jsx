de00405ad8d43096f3ccf4a69c4ee103693f5dcb
Pohe1999 <mapogu99@gmail.com>
2023-12-01 11:32:41 -0600

    update footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,11 @@ const Footer = () => {
 
     return(
         <div name="footer" className="bg-red-900 py-6 font-thin text-sm">
+            <p className="text-center text-white">Politicas de privacidad</p>
             <p className="text-center text-white">
                 @ 2023 Creado por: 
                 <a href="https://github.com/Pohe1999" target="_blank"> Mauricio Portillo Gutierrez</a>
             </p>
-            <p className="text-center text-white">Politicas de privacidad</p>
         </div>
     )
 }
