a5c638539e45de6654107ea2a9191221ed3a3ee6
Mauricio Portillo <106699864+Pohe1999@users.noreply.github.com>
2023-12-02 11:21:05 -0600

    Update Social.jsx

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -18,9 +18,6 @@ const Social = () => {
                 <li className='w-[250px] h-[55px] flex justify-between items-center ml-[-200px] hover:ml-[-180px] duration-300 bg-green-500 text-white rounded-lg'>
                     <p>WhatsApp</p><SendMessage  />
                 </li>
-                <li className='w-[250px] h-[55px] flex justify-between items-center text-center  ml-[-200px] hover:ml-[-180px] duration-300 rounded-lg border-2 border-black'>
-                    <a className='flex justify-end w-full text-black pe-2' href="https://es.wikipedia.org/wiki/Samuel_Hern%C3%A1ndez_Cruz" target='_blank'><FaWikipediaW className="text-4xl" /></a>
-                </li>
                 </ul>
             </div>
     </div>
