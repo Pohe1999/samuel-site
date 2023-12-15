9f5db9830cf2ea1405b697716df133b10de2abb0
Mauricio Portillo <106699864+Pohe1999@users.noreply.github.com>
2023-12-15 11:51:43 -0600

    Update Alerta.jsx

--- src/components/Alerta.jsx
+++ src/components/Alerta.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import DownloadButton from './DownloadButton';
+import { FaTimes } from 'react-icons/fa'; // Importa el ícono de cierre desde react-icons
 
 const AlertaTemporal = () => {
   const [mostrarAlerta, setMostrarAlerta] = useState(true);
@@ -6,11 +8,11 @@ const AlertaTemporal = () => {
   useEffect(() => {
     const timeoutMostrar = setTimeout(() => {
       setMostrarAlerta(true);
-    }, 1000); // 15 segundos en milisegundos
+    }, 10000); // 10 segundos en milisegundos
 
     const timeoutOcultar = setTimeout(() => {
       setMostrarAlerta(false);
-    }, 4000); // 5 segundos en milisegundos
+    }, 5000); // 5 segundos en milisegundos
 
     return () => {
       clearTimeout(timeoutMostrar);
@@ -18,18 +20,18 @@ const AlertaTemporal = () => {
     };
   }, []);
 
-  return (
-    <div className={`fixed opacity-95 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-gray-500 border-2 border-white shadow-2xl text-white p-4 rounded-xl z-50 ${mostrarAlerta ? 'block' : 'hidden'}`}>
-      <div className='text-center'>
-        <p className="font-bold pb-5">¡Hola!, te invito a que veas la entrevista que nos realizó <span className='border-b-2'>La Jornada:</span></p>
-        <p>Tecámac: Proyectan obras que garanticen agua alrededor del AIFA</p>
-      </div>
-      <div className='flex items-center text-center justify-center pt-5'>
-        <a className='text-center border-2 px-2 py-1 rounded-lg' href="https://lajornadaestadodemexico.com/tecamac-proyectan-obras-que-garanticen-agua-alrededor-del-aifa/?fbclid=IwAR2cYOD382A3NHsTvS7j4rbmdDiCyb3l-v0_IdRMTnXleWt5VU72WEomBf0&cn-reloaded=1" target='_blank'>
-            Ver publicacion
-        </a>
-      </div>
+  const cerrarAlerta = () => {
+    setMostrarAlerta(false);
+  };
 
+  return (
+    <div className={`fixed font-thin opacity-95 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-gray-500 border-2 border-white shadow-2xl text-white p-4 text-center rounded-xl z-50 ${mostrarAlerta ? 'block' : 'hidden'}`}>
+      <button className="absolute top-2 right-2" onClick={cerrarAlerta}>
+        <FaTimes /> {/* Ícono de cierre */}
+      </button>
+      <h4 className='text-2xl py-4'>8 Columnas</h4>
+      <h5 className='pb-4'>Garantizada la seguridad y servicios en tecámac ante los retos que enfrenta el AIFA.</h5>
+      <DownloadButton />
     </div>
   );
 };
