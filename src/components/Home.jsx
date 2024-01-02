4c7f7ee9705f833963878bf9df6048a4e42b379d
Mauricio Portillo <106699864+Pohe1999@users.noreply.github.com>
2024-01-02 13:53:38 -0600

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 const Home = () => {
   const [showVideo, setShowVideo] = useState(false);
+  const videoRef = useRef(null);
 
   const handlePlay = () => {
     setShowVideo(true);
+    if (videoRef.current) {
+      videoRef.current.contentWindow.postMessage(
+        '{"event":"command","func":"playVideo","args":""}',
+        '*'
+      );
+    }
   };
 
   return (
@@ -22,11 +29,12 @@ const Home = () => {
         <div className="sm:w-full sm:h-full w-full h-[210px] overflow-hidden">
           <iframe
             className="w-full h-full"
-            src="https://www.youtube.com/embed/yktq7IBkKXw?si=zS69JfaQDbsHhRKV&autoplay=1&modestbranding=1&controls=0"
+            src="https://www.youtube.com/embed/yktq7IBkKXw?enablejsapi=1&autoplay=0&modestbranding=1&controls=0"
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             allowFullScreen
+            ref={videoRef}
             style={{ width: '100%', height: '100%' }}
           ></iframe>
         </div>
